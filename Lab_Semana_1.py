6ca41e9ffcf37de8ddf3ad94941a13bc57f20310
Emanuel34w <emmanuelgom2.0@gmail.com>
2025-01-30 20:02:53 -0600

    Tarea, primera semana
    Esta es una tarea para la facultad

---
+++ Lab_Semana_1.py
@@ -0,0 +1,44 @@
+print("Instrucciones:Responde las siguentes preguntas y ejercicios relacionados con los conceptos basicos de la programacion. Asegurate de justificar tus respuestas cuando sea necesario y seguir con esl formato indicado: ")
+print("Ejercicio 1: ")
+print("¿Cual es la funcion de la memoria secundaria en una computadora?")
+print("Almacenar informacion durante mucho tiempo, incuso después de ciclos de apagado y encendido")
+
+print("Ejercicio 2: ")
+print("¿Qué es un programa?")
+print("Es una sencuencia de declaraciones o sentencias que han sido diseñadas para hacer algo")
+
+print("Ejercicio 3: ")
+print("¿Cuál es la diferencia entre un compilador y un intérprete?: ")
+print("El intérprete es un programa que lee y ejecuta el código fuente línea por línea, mientras que el compilador es un programa que traduce todo el código fuente a un lenguaje de máquina antes de su ejecución.")
+
+print("Ejercicio 4: ")
+print("¿Cuál de los siguientes contiene 'codigo maquina'?: ")
+print("El código fuente de Python")
+
+print("Ejercicio 5: ")
+print("¿Qué esta mal en este código?: ")
+print("Uno de los problemas es que escribio mal el print, en ves de eso se ecsibio primt, y en segunda se utilizo mal las comillas")
+
+print("Ejercicio 6: ")
+print("¿En qué lugar del computador queda almacenada una variable, como en este caso 'x' después de ejecutar la siguiente línea de Python?: ")
+"x=123"
+print("Memoria Secundaria")
+
+print("Ejercicio 7: ")
+print("¿Qué mostrará en pantalla el siguiente programa?: ")
+"x=43"
+"x=x+1"
+"print(X)"
+print("44")
+
+print("Ejercicio 8: ")
+print("Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:") 
+print("Unidad central de procesamiento:Seria el cerebro ya que es el encargado de interpretar información, tomar decisiones y coordinar las acciones del cuerpo, al igual que la CPU realiza cálculos, ejecuta instrucciones y dirige el funcionamiento de los componentes de una computadora Ejemplo: Si estás resolviendo un problema matemático, tu cerebro analiza los números y operaciones (procesamiento), toma decisiones sobre qué pasos seguir y da instrucciones al resto del cuerpo (como escribir la respuesta). Este proceso refleja cómo la CPU realiza cálculos y coordina tareas dentro de una computadora")
+print("Memoria principal: Sería la memoria a corto plazo.La memoria principal almacena información temporal que el cerebro o la computadora necesita usar en el momento. Sin embargo, esta información no se guarda permanentemente; desaparece cuando ya no es útil o cuando se interrumpe la actividad.Ejemplo:Cuando lees un número de teléfono y lo memorizas temporalmente para marcarlo, estás utilizando tu memoria a corto plazo. Una vez que realizas la llamada, es probable que olvides el número rápidamente. Esto se asemeja a cómo la RAM guarda datos mientras se están procesando, pero los pierde cuando se apaga la computadora o termina el proceso.")
+print("Memoria secundaria: sería la memoria a largo plazo.La memoria secundaria almacena información de forma permanente o semipermanente para que pueda ser recuperada cuando se necesite, tal como lo hace un disco duro en una computadora.Ejemplo:Recordar el nombre de tu mejor amigo de la infancia o la fecha de tu cumpleaños son ejemplos de memoria a largo plazo. Aunque no lo uses todos los días, esa información está guardada y puedes acceder a ella cuando sea necesario. Esto es similar a cómo los discos duros o unidades de estado sólido almacenan archivos, fotos o programas de forma permanente hasta que se decida eliminarlos. ")
+print("Dispositivo de entrada: serían los sentidos (vista, oído, tacto, gusto y olfato).Los dispositivos de entrada son los medios por los cuales una computadora recibe información del exterior, y en los humanos, los sentidos permiten captar estímulos del entorno para que el cerebro los procese.Ejemplo:Vista: Tus ojos captan la luz y las imágenes, como cuando lees un libro.Oído: Tus oídos reciben sonidos, como cuando escuchas a alguien hablar.Tacto: Tus manos sienten la textura de un objeto o el calor de una superficie.Esto es como un teclado o un mouse para una computadora, que le permiten recibir información que luego será procesada.")
+print("Dispositivo de salida: serían las acciones o expresiones externas, como hablar, escribir, gesticular o realizar movimientos.Los dispositivos de salida permiten que la computadora comunique o exprese la información procesada, y en los humanos, son las formas en que el cuerpo transmite información al mundo exterior.Ejemplo:Hablar: Responder a una pregunta verbalmente.Escribir: Tomar notas para compartir una idea con alguien.Gestos: Levantar el pulgar para indicar aprobación o mover la cabeza para asentir.Esto es como una pantalla o una impresora en una computadora, que muestran o materializan los resultados del procesamiento interno.")
+
+print("Ejercicio 9: ")
+print("¿Cómo corregir un 'Error de sintaxis'?: ")
+print("Un error de sintaxis significa qure has violado las reglas 'gramaticales', en este caso de Python, hace todo lo que puede para señalar el punto exacto, por ende, es recomdendable Lee el mensaje de error: Identifica la línea y el tipo de problema.Localiza el error: Busca paréntesis, comillas o indentaciones faltantes.Verifica las reglas: Asegúrate de que tu código siga la sintaxis correcta del lenguaje.Prueba por partes: Ejecuta el código en fragmentos pequeños para aislar el problema.Usa herramientas: IDEs o editores como VS Code pueden señalar errores automáticamente.")
